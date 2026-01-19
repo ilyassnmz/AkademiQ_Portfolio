ec5c02e6cab66d499110a45c3816d61bbc8c9d5c
Ilyas Sonmez <lunap.ilyas@gmail.com>
2026-01-19 21:44:16 +0300

    DefaultController ve birden fazla View Component partial eklendi.

---
+++ Portfolio/Portfolio/Portfolio/ViewComponents/_DefaultEducationComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.ViewComponents
+{
+    public class _DefaultEducationComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+            { return View(); }
+    }
+}
